78409db1595368ba2e26c7419d1d5d4c1f97c4f2
wallarug <wallarug@coliemore.com.au>
2020-11-07 22:34:50 +1100

    copied sdsandbox into this new repo

---
+++ sdsim/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,416 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using tk;
+using System;
+
+public class CarSpawner : MonoBehaviour {
+
+	public GameObject carPrefab;
+	public Transform[] startsTm; // list containing multiple starting points
+    public bool EnableTrainingManager = false;
+
+	public delegate void OnNewCar(GameObject carObj);
+	public OnNewCar OnNewCarCB;	
+
+    public int numCarRows = 2;
+    public float distCarCols = 4.5f;
+    public float distCarRows = 5f;
+
+	public Camera splitScreenCamLeft;
+	public Camera splitScreenCamRight;
+
+    public GameObject racerStatusPrefab;
+    public RectTransform raceStatusPanel;
+    int raceStatusWidth = 380;
+    int raceStatusHeight = 100;
+    int n_columns = 2; // number of columns in the RaceStatus panel
+
+	private List<GameObject> cars = new List<GameObject>();
+
+	static public GameObject getChildGameObject(GameObject fromGameObject, string withName) {
+         //Author: Isaac Dart, June-13.
+         Transform[] ts = fromGameObject.transform.GetComponentsInChildren<Transform>(true);
+         foreach (Transform t in ts) if (t.gameObject.name == withName) return t.gameObject;
+
+        Debug.LogError("couldn't find: " + withName);
+         return null;
+    }
+
+    // Find the car for the given JsonTcpClient and remove it from the scene.
+    public bool RemoveCar(tk.JsonTcpClient client)
+    {
+        GameObject toRemove = null;
+
+        foreach(GameObject go in cars)
+        {
+            GameObject TcpClientObj = getChildGameObject(go, "TCPClient");
+
+            if(TcpClientObj != null)
+            {
+                tk.TcpCarHandler handler = TcpClientObj.GetComponent<tk.TcpCarHandler>();
+                
+                if(handler != null && handler.GetClient() == client)
+                {
+                    toRemove = go;
+                }
+            }
+        }
+
+        if(toRemove != null)
+        {   
+            RemoveTimer(toRemove);
+            cars.Remove(toRemove);
+            GameObject.Destroy(toRemove);
+
+            if (cars.Count < 2)
+                DeactivateSplitScreen();
+
+            Debug.Log("Removed car");
+            return true;
+        }
+        else
+        {
+            Debug.LogError("failed to remove car");
+            return false;
+        
+        }
+    }
+
+    public void RemoveAllCars()
+    {
+        foreach(GameObject car in cars)
+        {
+            RemoveTimer(car);
+            GameObject.Destroy(car);
+        }
+
+        cars.Clear();
+        DeactivateSplitScreen();
+        RemoveUiReferences();
+    }
+
+    void UpdateRaceStatusPannel()
+    {
+        int n_children = raceStatusPanel.transform.childCount;
+        int row = n_children;
+        if (row > n_columns)
+            row = n_columns;
+
+        int col = (n_children / n_columns) + (n_children % n_columns);
+        float width = row * raceStatusWidth;
+        float height = col * raceStatusHeight;
+        raceStatusPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,  width);
+        raceStatusPanel.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical,  height);
+        raceStatusPanel.anchoredPosition = new Vector3(8.0f, -1 * height, 0.0f);
+    }
+
+    public void AddTimer(Timer t, tk.JsonTcpClient client)
+    {
+        if(racerStatusPrefab == null)
+            return;
+
+        GameObject go = Instantiate(racerStatusPrefab) as GameObject;
+        RaceStatus rs = go.GetComponent<RaceStatus>();
+        rs.Init(t, client);
+        go.transform.SetParent(raceStatusPanel.transform);
+        
+        UpdateRaceStatusPannel(); // update the UI with the new child count
+        Debug.Log("Added timer");
+        
+    }
+    
+    public void RemoveTimer(GameObject go)
+    {   
+        Timer timer = getChildGameObject(go, "Timer").GetComponent<Timer>();
+        
+        if(timer != null && raceStatusPanel != null){
+            int count = raceStatusPanel.transform.childCount;
+            for(int i = 0; i < count; i++)
+            {
+                Transform child = raceStatusPanel.transform.GetChild(i);
+                RaceStatus rs = child.GetComponent<RaceStatus>();
+                if(rs.timer == timer)
+                {   
+                    child.transform.parent = null; // detach from parent
+                    Destroy(child.gameObject); // destroy child
+                    UpdateRaceStatusPannel(); // update the UI with the new child count
+                    Debug.Log("removed timer");
+                    return;
+                }
+            }
+            Debug.LogError("failed to find timer while removing it");
+            return;
+        }
+        Debug.LogError("failed to remove timer");
+    }
+
+    public Camera ActivateSplitScreen()
+    {
+        Camera cam = Camera.main;
+
+        if (splitScreenCamLeft != null)
+        {
+            splitScreenCamLeft.gameObject.SetActive(true);
+            cam = splitScreenCamLeft;
+        }
+
+        if (splitScreenCamRight != null)
+        {
+            splitScreenCamRight.gameObject.SetActive(true);
+            Transform camFollowRt = getChildGameObject(cars[0], "CameraFollowTm").transform;
+            CameraFollow rtCameraFollow = splitScreenCamRight.transform.GetComponent<CameraFollow>();
+            rtCameraFollow.target = camFollowRt;
+        }
+
+        return cam;
+    }
+
+    public void DeactivateSplitScreen()
+    {
+        Camera cam = Camera.main;
+
+        if (splitScreenCamLeft != null)
+        {
+            splitScreenCamLeft.gameObject.SetActive(false);
+            cam = splitScreenCamLeft;
+        }
+
+        if (splitScreenCamRight != null)
+        {
+            splitScreenCamRight.gameObject.SetActive(false);
+        }
+    }
+
+    public void CarTextFacecamera(GameObject car, Transform target)
+    {
+        GameObject carNameObj = getChildGameObject(car, "CarName");
+
+        if(!carNameObj)
+            return;
+
+        FaceTarget ft = carNameObj.GetComponent<FaceTarget>();
+
+        if(!ft)
+            return;
+
+        ft.target = target;
+    
+    }
+
+    public bool IsOccupied(Vector3 pos)
+    {
+        int carCount = cars.Count - 1;
+
+        for(int iCar = 0; iCar < carCount; iCar++)
+        {
+            if(Vector3.Distance(cars[iCar].transform.position, pos) < 1.0f)
+                return true;
+        }
+
+        return false;
+    }
+
+    public (Vector3, Quaternion) GetStartPosRot(int iCar)
+    {   
+
+        int iSpawn = iCar % startsTm.Length;
+        int iCol = (iCar / startsTm.Length) % numCarRows;
+        int iRow = (iCar / startsTm.Length) / numCarRows;
+
+        Vector3 offset = Vector3.zero;
+        offset += Vector3.forward * distCarRows * iRow;
+        offset += Vector3.left * distCarCols * iCol;
+
+        Transform spawn = startsTm[iSpawn];
+        Vector3 pos = spawn.position + offset;
+        Quaternion rot = spawn.rotation;
+        return (pos, rot);
+    }
+
+    public (Vector3, Quaternion) GetCarStartPosRot()
+    {   
+
+        Vector3 startPos = startsTm[0].position; // default position
+        Quaternion startRot = startsTm[0].rotation; // default rotation
+
+        if (IsOccupied(startPos))
+        {
+            int iCar = 0;
+            while(IsOccupied(startPos))
+            {   
+                (startPos, startRot) = GetStartPosRot(iCar);
+                iCar++;
+            }
+        }
+
+        return (startPos, startRot);
+    }
+
+
+    public GameObject Spawn (tk.JsonTcpClient client) 
+	{
+        if(carPrefab == null)
+        {
+            Debug.LogError("No carPrefab set in CarSpawner!");
+            return null;
+        }
+
+        // Create a car object, and also hook up all the connections
+        // to various places in game that need to hook into the car.
+		GameObject go = GameObject.Instantiate(carPrefab) as GameObject;
+
+        if (go == null)
+        {
+            Debug.LogError("CarSpawner failed to instantiate prefab!");
+            return null;
+        }
+
+        cars.Add(go);
+        
+        (Vector3 startPos, Quaternion startRot) = GetCarStartPosRot();
+        go.transform.position = startPos;
+        go.transform.rotation = startRot;
+        go.GetComponent<Car>().SavePosRot();
+
+		GameObject TcpClientObj = getChildGameObject(go, "TCPClient");
+
+        Camera cam = Camera.main;
+
+		// Detect that we have the second car. Doesn't really handle more than 2 right now.
+		if(cars.Count > 1)
+		{
+            cam = ActivateSplitScreen();
+		}
+
+        // CarTextFacecamera(go, cam.transform);
+
+		if(TcpClientObj != null)
+		{
+			// without this it will not connect.
+			TcpClientObj.SetActive(true);
+
+			// now set the connection settings.
+			TcpCarHandler carHandler = TcpClientObj.GetComponent<TcpCarHandler>();
+
+            if (carHandler != null)
+                carHandler.Init(client);
+		}
+
+        if (OnNewCarCB != null)
+			OnNewCarCB.Invoke(go);
+
+        ///////////////////////////////////////////////
+        // Search scene to find these.
+        CameraFollow cameraFollow = cam.transform.GetComponent<CameraFollow>();
+        MenuHandler menuHandler = GameObject.FindObjectOfType<MenuHandler>();
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        GameObject panelMenu = getChildGameObject(canvas.gameObject, "Panel Menu");
+        PID_UI pid_ui = null;
+        GameObject pidPanel = getChildGameObject(canvas.gameObject, "PIDPanel");
+        ///////////////////////////////////////////////
+
+        if (pidPanel)
+            pid_ui = pidPanel.GetComponent<PID_UI>();
+
+        // set camera target follow tm
+        if (cameraFollow != null)
+			cameraFollow.target = getChildGameObject(go, "CameraFollowTm").transform;
+
+        // Set menu handler hooks
+		if(menuHandler != null)
+		{
+			menuHandler.PIDContoller = getChildGameObject(go, "PIDController");
+			menuHandler.Logger = getChildGameObject(go, "Logger");
+			menuHandler.NetworkSteering = getChildGameObject(go, "TCPClient");
+			menuHandler.carJSControl = getChildGameObject(go, "JoyStickCarContoller");
+			menuHandler.trainingManager = getChildGameObject(go, "TrainingManager").GetComponent<TrainingManager>();
+            menuHandler.trainingManager.carObj = go;
+
+            if (EnableTrainingManager)
+            {
+                menuHandler.trainingManager.gameObject.SetActive(true);
+
+                getChildGameObject(go, "OverheadViewSphere").SetActive(true);
+            }
+
+            if (GlobalState.bAutoHideSceneMenu && panelMenu != null)
+            {
+                panelMenu.SetActive(false);
+            }
+
+        }
+
+        // Set the PID ui hooks
+		if (pid_ui != null)
+		{
+			pid_ui.pid = getChildGameObject(go, "PIDController").GetComponent<PIDController>();
+			pid_ui.logger = getChildGameObject(go, "Logger").GetComponent<Logger>();
+		}
+		else
+		{
+			Debug.LogError("failed to find PID_UI");
+		}
+        
+        // Add race status, if possible.
+        GameObject to = getChildGameObject(go, "Timer");
+
+        if(to != null)
+        {
+            AddTimer(to.GetComponent<Timer>(), client);
+        }   
+		else
+		{
+			Debug.LogError("failed to find Timer");
+		}
+
+        return go;
+    }
+
+    internal void EnsureOneCar()
+    {
+        if (cars.Count == 0)
+            Spawn(null);
+    }
+
+    public void RemoveUiReferences()
+    {
+        Camera cam = Camera.main;
+
+        ///////////////////////////////////////////////
+        // Search scene to find these.
+        CameraFollow cameraFollow = cam.transform.GetComponent<CameraFollow>();
+        MenuHandler menuHandler = GameObject.FindObjectOfType<MenuHandler>();
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        GameObject panelMenu = getChildGameObject(canvas.gameObject, "Panel Menu");
+        PID_UI pid_ui = null;
+        GameObject pidPanel = getChildGameObject(canvas.gameObject, "PIDPanel");
+        ///////////////////////////////////////////////
+
+        if (pidPanel)
+            pid_ui = pidPanel.GetComponent<PID_UI>();
+
+        // set camera target follow tm
+        if (cameraFollow != null)
+			cameraFollow.target = null;
+
+        // Set menu handler hooks
+		if(menuHandler != null)
+		{
+			menuHandler.PIDContoller = null;
+			menuHandler.Logger = null;
+			menuHandler.NetworkSteering = null;
+			menuHandler.carJSControl  = null;
+			menuHandler.trainingManager  = null;
+        }
+
+        // Set the PID ui hooks
+		if (pid_ui != null)
+		{
+			pid_ui.pid = null;
+			pid_ui.logger = null;
+		}
+
+    }
+	
+}
---
+++ sdsim/Assets/Scripts/Lidar.cs
@@ -0,0 +1,308 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class LidarPoint
+{
+    public float x;
+    public float y;
+    public float z;
+    public float rx;
+    public float ry;
+
+	public LidarPoint(Vector3 p, float _rx, float _ry)
+	{
+		x = p.x;
+		y = p.y;
+		z = p.z;
+        rx = _rx;
+        ry = _ry;
+	}
+}
+
+[Serializable]
+public class V3
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public V3(Vector3 p)
+    {
+        x = p.x;
+        y = p.y;
+        z = p.z;
+    }
+}
+
+[Serializable]
+public class LidarPointArray
+{
+	//All coordinates in World coordinate system
+
+	public V3 lidarPos;
+
+	public V3 lidarOrientation;
+
+    public LidarPoint[] points;
+
+    public void Init(int numPoints)
+    {
+        points = new LidarPoint[numPoints];
+    }
+}
+
+public class Lidar : MonoBehaviour {
+
+
+	LidarPointArray pointArr;
+
+	//as the ray sweeps around, how many degrees does it advance per sample
+	public int degPerSweepInc = 2;
+
+	//what is the starting angle for the initial sweep compared to the forward vector
+	public float degAngDown = 25f;
+
+	//what angle change between sweeps 
+	public float degAngDelta = -1f;
+
+	//how many complete 360 sweeps
+	public int numSweepsLevels = 25;
+
+	//what it max distance we will register a hit
+	public float maxRange = 50f;
+
+	//how large radius to use when rendering debug display
+	public float gizmoSize = 0.1f;
+
+	//what is the scalar on the perlin noise applied to point position
+	public float noise = 0.2f;
+
+	public bool DisplayDebugInScene = false;
+
+    // are there layers we don't want to collide with?
+    public string[] layerMaskNames;
+
+    int collMask = 0;
+
+	void Awake()
+	{
+		pointArr = new LidarPointArray();
+		pointArr.Init(360 / degPerSweepInc * numSweepsLevels);
+
+        int v = 0;
+
+        foreach (string layerName in layerMaskNames)
+        {
+            int layer = LayerMask.NameToLayer(layerName);
+            v |= 1 << layer;
+        }
+
+        collMask |= ~v;
+
+    }
+
+    public void SetConfig(float offset_x, float offset_y, float offset_z, float rot_x,
+        int _degPerSweepInc, float _degAngDown, float _degAngDelta, float _maxRange, float _noise, int _numSweepsLevels)
+    {
+        degPerSweepInc = _degPerSweepInc;
+        degAngDown = _degAngDown;
+        degAngDelta = _degAngDelta;
+        maxRange = _maxRange;
+        noise = _noise;
+        numSweepsLevels = _numSweepsLevels;
+
+        if (offset_x != 0.0f || offset_y != 0.0f || offset_z != 0.0f)
+            transform.localPosition = new Vector3(offset_x, offset_y, offset_z);
+
+        if (rot_x != 0.0f)
+            transform.localEulerAngles = new Vector3(rot_x, 0.0f, 0.0f);
+
+        pointArr = new LidarPointArray();
+        pointArr.Init(360 / degPerSweepInc * numSweepsLevels);
+    }
+
+    public JSONObject GetOutputAsJson()
+    {
+        LidarPointArray points = GetOutput();
+        JSONObject json = JSONObject.Create();
+        foreach(LidarPoint p in points.points)
+        {
+            JSONObject vec = JSONObject.Create();
+            try
+            {
+                vec.AddField("x", p.x);
+                vec.AddField("y", p.y);
+                vec.AddField("z", p.z);
+                vec.AddField("rx", p.rx);
+                vec.AddField("ry", p.ry);
+                json.Add(vec);
+            }
+            catch
+            {
+                // just ignore points that don't resolve.
+            }
+        }
+        
+        return json;
+    }
+    
+	public LidarPointArray GetOutput()
+	{
+		pointArr = new LidarPointArray();
+		pointArr.Init(360 / degPerSweepInc * numSweepsLevels);
+
+		pointArr.lidarPos = new V3(transform.position);
+		pointArr.lidarOrientation = new V3(transform.rotation.eulerAngles);
+		
+		Ray ray = new Ray();
+
+		ray.origin = this.transform.position;
+		ray.direction = this.transform.forward;
+
+		//start out pointing a bit down.
+		Quaternion rotDown = Quaternion.AngleAxis(degAngDown, transform.right);
+		ray.direction = rotDown * ray.direction;
+
+		int numSweep = 360 / degPerSweepInc;
+
+		Quaternion rotSide = Quaternion.AngleAxis(degPerSweepInc, transform.up);
+		Quaternion rotUp = Quaternion.AngleAxis(degAngDelta, transform.right);
+
+		RaycastHit hit;
+
+		//Sample the output texture to create rays.
+		int iP = 0;
+        float rx = 0.0f;
+        float ry = 0.0f;
+
+		for(int iS = 0; iS < numSweepsLevels; iS++)
+		{
+			for(int iA = 0; iA < numSweep; iA++)
+			{               
+				if(Physics.Raycast(ray, out hit, maxRange, collMask))
+				{
+					//sample that ray at the depth given by the pixel.
+					Vector3 pos = ray.GetPoint(hit.distance);
+
+                    //make points relative to my pos.
+                    pos = pos - transform.position;
+
+					//shouldn't hit this unless user is messing around in the interface with things running.
+					if(iP >= pointArr.points.Length)
+						break;
+
+					float noiseX = Mathf.PerlinNoise(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+					float noiseY = Mathf.PerlinNoise(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+					float noiseZ = Mathf.PerlinNoise(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+					pos.x += noise * noiseX;
+					pos.y += noise * noiseY;
+					pos.z += noise * noiseZ;
+
+					//set iPoint
+					pointArr.points[iP] = new LidarPoint(pos, rx, ry);
+
+					ray.direction = rotSide * ray.direction;
+
+					iP++;
+				}
+
+                rx += degPerSweepInc;
+            }
+
+			ray.direction = rotUp * ray.direction;
+            ry += degAngDelta;
+            rx = 0.0f;
+        }
+
+		return pointArr;
+	}
+
+	void OnDrawGizmosSelected() 
+	{		
+		Gizmos.color = Color.red;
+
+		LidarPointArray arr = GetOutput();
+
+		if(arr == null)
+			return;
+		
+		Vector3 pos = Vector3.zero;
+
+		foreach(LidarPoint p in arr.points)
+		{
+			if(p == null)
+				continue;
+			
+			pos.x = p.x;
+			pos.y = p.y;
+			pos.z = p.z;
+
+            //make points global space for drawing
+            pos = transform.position + pos;
+
+            Gizmos.DrawSphere(pos, gizmoSize);
+		}
+	}
+
+	static Material lineMaterial;
+	static void CreateLineMaterial ()
+	{
+		if (!lineMaterial)
+		{
+			// Unity has a built-in shader that is useful for drawing
+			// simple colored things.
+			Shader shader = Shader.Find ("Hidden/Internal-Colored");
+			lineMaterial = new Material (shader);
+			lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+			// Turn on alpha blending
+			lineMaterial.SetInt ("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+			lineMaterial.SetInt ("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+			// Turn backface culling off
+			lineMaterial.SetInt ("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+			// Turn off depth writes
+			lineMaterial.SetInt ("_ZWrite", 0);
+		}
+	}
+
+	// Will be called after all regular rendering is done
+	public void OnRenderObject ()
+	{
+
+		if(DisplayDebugInScene == false)
+			return;
+		
+		CreateLineMaterial ();
+		// Apply the line material
+		lineMaterial.SetPass (0);
+
+		GL.PushMatrix ();
+		// Set transformation matrix for drawing to
+		// match our transform
+		//GL.MultMatrix (transform.localToWorldMatrix);
+		GL.MultMatrix(Matrix4x4.identity);
+		// Draw lines
+		GL.Begin (GL.LINES);
+
+		LidarPointArray arr = GetOutput();
+
+		if(arr == null)
+			return;
+		
+		foreach(LidarPoint p in arr.points)
+		{
+			if(p == null)
+				continue;
+
+			//red
+			GL.Color (new Color (1, 0, 0, 0.8F));
+			GL.Vertex3(p.x, p.y, p.z);
+			GL.Vertex3(p.x, p.y + gizmoSize, p.z);
+		}
+
+		GL.End ();
+		GL.PopMatrix ();
+	}
+}
