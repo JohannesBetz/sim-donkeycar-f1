b158a3b274e69464cbf6e8bd8467644a1e1a3680
wallarug <wallarug@coliemore.com.au>
2020-12-03 23:06:13 +1100

    added robotics masters chellange and race tracks.  Added traffic sign assets

---
+++ sdsim/Assets/Traffic Signs/Traffic Light/RedSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedSwitch : MonoBehaviour
+{
+    public Material[] materials = new Material[2];
+    public Renderer rend;
+    private int LightState = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+             LightState++;
+             if(LightState > 3){
+                 LightState = 0;
+             }
+        }
+        
+        if(LightState == 1){
+            rend.sharedMaterial = materials[0];
+        } else {
+            rend.sharedMaterial = materials[1];
+        }
+    }
+}
---
+++ sdsim/Assets/Traffic Signs/Traffic Light/ToggleLights.cs
@@ -0,0 +1,24 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+
+ public class ToggleLights : MonoBehaviour
+ {
+     //Start traffic light in off state
+     public int LightState = 0;
+     void Start()
+     {
+     }
+    //Change state when 'E' is pressed
+     void Update()
+     {
+         //Toggles between traffic light states: 0=Off, 1=Red, 2=Yellow, 3=Green
+         if (Input.GetKeyDown(KeyCode.E)){
+             LightState++;
+             if(LightState > 3){
+                 LightState = 0;
+             }
+        }
+     }  
+
+ }
